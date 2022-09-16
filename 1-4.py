3a0c5261c5c77183fbd91e477cf79d9a82d5f249
juun9714 <lovekwon200@gmail.com>
2022-09-16 15:53:17 +0900

    comment deleted

--- 1-4.py
+++ 1-4.py
@@ -1,5 +1,6 @@
 # 2022-2 Machine learning with Graphs 2022711835 Junhee Kwon
 # Homework 1
+# 1.4
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -11,23 +12,21 @@ from matplotlib import pyplot as plt
 
 G = nx.karate_club_graph()
 
-# 1.4
-graph=nx.karate_club_graph()
-graph=nx.relabel_nodes(graph, {n:str(n) for n in graph.nodes()})
+G=nx.karate_club_graph()
+G=nx.relabel_nodes(G, {n:str(n) for n in G.nodes()})
 
-node2vec = Node2Vec(graph=graph, dimensions=64, walk_length=10, p=0.5, q=0.5, num_walks=200, workers=1) 
+node2vec = Node2Vec(graph=G, dimensions=64, walk_length=30, p=0.5, q=0.5, num_walks=200, workers=1) 
 model = node2vec.fit(window=10, min_count=1, batch_words=4)  
 
 K = 3
 kmeans = KMeans(n_clusters=K, random_state=0).fit(model.wv.vectors)
 
 for n, label in zip(model.wv.index_to_key, kmeans.labels_):
-    graph.nodes[n]['label'] = label
-label=[n[1]['label'] for n in graph.nodes(data=True)]
-print(label)
+    G.nodes[n]['label'] = label
+label=[n[1]['label'] for n in G.nodes(data=True)]
 
 plt.figure(figsize=(12, 6))
-nx.draw_networkx(graph, pos=nx.layout.spring_layout(graph), 
+nx.draw_networkx(G, pos=nx.layout.spring_layout(G), 
                  node_color=label, 
                  cmap=plt.cm.rainbow
                 )
